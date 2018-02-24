239edf677566c875bd0c1abc5d58c46ac33f5451
Ian Riley <ian.riley.915@gmail.com>
2018-02-23 23:58:24 -0600

    Outlines encrypt and decrypt.

--- aes.c
+++ aes.c
@@ -1,9 +1,72 @@
-void encrypt(unsigned int** state, unsigned int* key, int rounds);
-void addRoundKey(unsigned int** state, unsigned int* key, int ki);
+void encrypt(unsigned int** state, unsigned int* key, int len);
+void addRoundKey(unsigned int** state, unsigned int* key, int keyi);
 void subBytes(unsigned int** state);
 void shiftRows(unsigned int** state);
 void mixColumns(unsigned int** state);
-void decrypt(unsigned int** state, unsigned int* key, int rounds);
-void invMixColumns(unsigned int** state);
-void invShiftRows(unsigned int** state);
+void decrypt(unsigned int** state, unsigned int* key, int len);
 void invSubBytes(unsigned int** state);
+void invShiftRows(unsigned int** state);
+void invMixColumns(unsigned int** state);
+
+void addRoundKey(unsigned int** state, unsigned int* key, int keyi) {
+
+}
+
+void subBytes(unsigned int** state) {
+
+}
+
+void shiftRows(unsigned int** state) {
+
+}
+
+void mixColumns(unsigned int** state) {
+
+}
+
+void encrypt(unsigned int** state, unsigned int* key, int len) {
+    int keyi = 0;
+    addRoundKey(state, key, keyi);
+    keyi += 4;
+    int rounds = len/4 - 2;
+    for (int i = 0; i < rounds; i++) {
+        subBytes(state);
+        shiftRows(state);
+        mixColumns(state);
+        addRoundKey(state, key, keyi);
+        keyi += 4;
+    }
+    subBytes(state);
+    shiftRows(state);
+    mixColumns(state);
+    addRoundKey(state, key, keyi);
+}
+
+void invSubBytes(unsigned int** state) {
+
+}
+
+void invShiftRows(unsigned int** state) {
+
+}
+
+void invMixColumns(unsigned int** state) {
+
+}
+
+void decrypt(unsigned int** state, unsigned int* key, int len) {
+    int keyi = len-4;
+    addRoundKey(state, key, keyi);
+    keyi -= 4;
+    int rounds = len/4 - 2;
+    for (int i = 0; i < rounds; i++) {
+        invShiftRows(state);
+        invSubBytes(state);
+        addRoundKey(state, key, keyi);
+        keyi -= 4;
+        invMixColumns(state);
+    }
+    invShiftRows(state);
+    invSubBytes(state);
+    addRoundKey(state, key, keyi);
+}
--- aes.h
+++ aes.h
@@ -1,9 +1,9 @@
-void encrypt(unsigned int** state, unsigned int* key, int rounds);
-void addRoundKey(unsigned int** state, unsigned int* key, int ki);
+void encrypt(unsigned int** state, unsigned int* key, int len);
+void addRoundKey(unsigned int** state, unsigned int* key, int keyi);
 void subBytes(unsigned int** state);
 void shiftRows(unsigned int** state);
 void mixColumns(unsigned int** state);
-void decrypt(unsigned int** state, unsigned int* key, int rounds);
-void invMixColumns(unsigned int** state);
-void invShiftRows(unsigned int** state);
+void decrypt(unsigned int** state, unsigned int* key, int len);
 void invSubBytes(unsigned int** state);
+void invShiftRows(unsigned int** state);
+void invMixColumns(unsigned int** state);
